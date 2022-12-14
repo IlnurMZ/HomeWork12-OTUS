c480ad4584ac9093462af76a278b9ecf715fc041
MukIlnur <24mi@bk.ru>
2022-12-14 15:53:39 +0500

    Добавил проект с библиотекой

---
+++ Jacks house/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Jacks_house
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Librarian/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Concurrent;
+
+namespace Librarian
+{
+    internal class Program
+    {
+        public static ConcurrentDictionary<string, int> library = new ConcurrentDictionary<string, int>();
+        static void Main(string[] args)
+        {
+            Task.Run(CalculateRead);
+            int choise = 0;
+            do
+            {
+                PrintMenu();
+                bool isCorrectChoise = int.TryParse(Console.ReadLine(), out choise);
+                if (!isCorrectChoise)
+                {
+                    continue;
+                }
+
+                switch (choise)
+                {
+                    case 1:
+                        AddBook();
+                        break;
+                    case 2:
+                        PrintAllBooks();
+                        break;                        
+                }
+            } while (choise != 3);            
+            Console.WriteLine("Вы вышли из программы");
+        }
+
+        public static void PrintMenu()
+        {
+            Console.WriteLine("1 - добавить книгу");
+            Console.WriteLine("2 - вывести список непрочитанного");
+            Console.WriteLine("3 - выйти");
+        }
+
+        public static void AddBook()
+        {
+            Console.WriteLine("Введите название книги");
+            string book = Console.ReadLine();
+            library.TryAdd(book, 0);
+        }
+
+        public static void PrintAllBooks()
+        {
+            foreach (var item in library)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }            
+        }
+
+        public static async Task CalculateRead()
+        {
+            while (true)
+            {
+                foreach (var item in library)
+                {
+                    //Console.WriteLine($"Status: {item.Key} - {item.Value}");
+                    library[item.Key]++;
+                    await Task.Delay(500);
+                    if (library[item.Key] >= 100)
+                    {
+                        library.TryRemove(item.Key, out int value);
+                    }
+                }
+            }            
+        }
+    }
+}
