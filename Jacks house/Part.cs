106fb9e10c60c1ffdba202579f7c0d46262b0701
MukIlnur <24mi@bk.ru>
2022-12-14 16:51:49 +0500

    Добавил код в проекте Jacks house

---
+++ Jacks house/Part.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jacks_house
+{
+    public class Part1
+    {
+        public List<string> Poem { get; set; } = new List<string>();
+        public void AddPart(ImmutableList<string> list)
+        {
+            string part1 = "Вот дом,\r\n" +
+                "Который построил Джек.";
+        }
+    }
+
+    public class Part2
+    {
+        public List<string> Poem { get; set; } = new List<string>();
+        public void AddPart(ImmutableList<string> list)
+        {
+            string part2 = "А это пшеница,\r\n" +
+                "Которая в темном чулане хранится\r\n" +
+                "В доме,\r\n" +
+                "Который построил Джек.";
+        }
+    }
+
+    public class Part3
+    {
+        public List<string> Poem { get; set; } = new List<string>();
+        public void AddPart(ImmutableList<string> list)
+        {
+            string part3 = "А это веселая птица-синица,\r\n" +
+                "Которая часто ворует пшеницу,\r\n" +
+                "Которая в темном чулане хранится\r\n" +
+                "В доме,\r\n" +
+                "Который построил Джек.";
+        }
+    }
+
+
+    public class Part4
+    {
+        public List<string> Poem { get; set; } = new List<string>();
+        public void AddPart(ImmutableList<string> list)
+        {
+            string part4 = "Вот кот,\r\n" +
+                "Который пугает и ловит синицу,\r\n" +
+                "Которая часто ворует пшеницу,\r\n" +
+                "Которая в темном чулане хранится\r\n" +
+                "В доме,\r\n" +
+                "Который построил Джек.";
+        }
+    }
+
+    public class Part5
+    {
+        public List<string> Poem { get; set; } = new List<string>();
+        public void AddPart(ImmutableList<string> list)
+        {
+            string part5 = "Вот пес без хвоста,\r\n" +
+                "Который за шиворот треплет кота,\r\n" +
+                "Который пугает и ловит синицу,\r\n" +
+                "Которая часто ворует пшеницу,\r\n" +
+                "Которая в темном чулане хранится\r\n" +
+                "В доме,\r\n" +
+                "Который построил Джек.";
+        }
+    }
+
+    public class Part6
+    {
+        public List<string> Poem { get; set; } = new List<string>();
+        public void AddPart(ImmutableList<string> list)
+        {
+            string part6 = "А это корова безрогая,\r\n" +
+                "Лягнувшая старого пса без хвоста,\r\n" +
+                "Который за шиворот треплет кота,\r\n" +
+                "Который пугает и ловит синицу,\r\n" +
+                "Которая часто ворует пшеницу,\r\n" +
+                "Которая в темном чулане хранится\r\n" +
+                "В доме,\r\n" +
+                "Который построил Джек.";
+        }
+    }
+
+    public class Part7
+    {
+        public List<string> Poem { get; set; } = new List<string>();
+        public void AddPart(ImmutableList<string> list)
+        {
+            string part7 = "А это старушка, седая и строгая,\r\n" +
+                "Которая доит корову безрогую,\r\n" +
+                "Лягнувшую старого пса без хвоста,\r\n" +
+                "Который за шиворот треплет кота,\r\n" +
+                "Который пугает и ловит синицу,\r\n" +
+                "Которая часто ворует пшеницу,\r\n" +
+                "Которая в темном чулане хранится\r\n" +
+                "В доме,\r\n" +
+                "Который построил Джек.";
+        }
+    }
+
+    public class Part8
+    {
+        public List<string> Poem { get; set; } = new List<string>();
+        public void AddPart(ImmutableList<string> list)
+        {
+            string part8 = "А это ленивый и толстый пастух,\r\n" +
+                "Который бранится с коровницей строгою,\r\n" +
+                "Которая доит корову безрогую,\r\n" +
+                "Лягнувшую старого пса без хвоста,\r\n" +
+                "Который за шиворот треплет кота,\r\n" +
+                "Который пугает и ловит синицу,\r\n" +
+                "Которая часто ворует пшеницу,\r\n" +
+                "Которая в темном чулане хранится\r\n" +
+                "В доме,\r\n" +
+                "Который построил Джек.";
+        }
+    }
+
+    public class Part9
+    {
+        public List<string> Poem { get; set; } = new List<string>();
+        public void AddPart(ImmutableList<string> list)
+        {
+            string part9 = "Вот два петуха,\r\n" +
+                "Которые будят того пастуха,\r\n" +
+                "Который бранится с коровницей строгою,\r\n" +
+                "Которая доит корову безрогую,\r\n" +
+                "Лягнувшую старого пса без хвоста,\r\n" +
+                "Который за шиворот треплет кота,\r\n" +
+                "Который пугает и ловит синицу,\r\n" +
+                "Которая часто ворует пшеницу,\r\n" +
+                "Которая в темном чулане хранится\r\n" +
+                "В доме,\r\n" +
+                "Который построил Джек.";
+        }
+    }
+}
--- Jacks house/Program.cs
+++ Jacks house/Program.cs
@@ -1,7 +1,10 @@
-﻿namespace Jacks_house
+﻿using System.Collections.Immutable;
+
+namespace Jacks_house
 {
     internal class Program
     {
+        private static readonly ImmutableList<string> Empty;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
